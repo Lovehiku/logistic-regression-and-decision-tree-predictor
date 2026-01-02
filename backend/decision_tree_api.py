c20c3795945de23bd19760dc90d6858b20d9f796
Lovehiku <hikulove2@gmail.com>
2026-01-02 17:20:19 +0300

    final commit

--- backend/decision_tree_api.py
+++ backend/decision_tree_api.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["https://decision-tree-api-xyz.onrender.com/predict", "http://localhost:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- backend/logistic_api.py
+++ backend/logistic_api.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # ✅ CORS (for Live Server)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["https://logistic-regression-and-decision-tree-9hd7.onrender.com/predict", "http://localhost:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
